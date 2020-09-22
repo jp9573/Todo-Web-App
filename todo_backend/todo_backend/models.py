3f2cb461190c635e83a25f5a259d271f632b6065
jp9573 <jp9573@gmail.com>
2020-09-22 19:20:36 +0530

    Added Backend APIs

--- todo_backend/todo_backend/models.py
+++ todo_backend/todo_backend/models.py
@@ -5,3 +5,6 @@ class Todos(models.Model):
     todo = models.TextField()
     created_ts = models.DateTimeField(auto_now_add=True)
     last_modified_ts = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.todo) + ' added at ' + str(self.created_ts)
--- todo_backend/todo_backend/views.py
+++ todo_backend/todo_backend/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Todos
 from rest_framework import status
+from django.core import serializers
 
 
 class TodoHandler(APIView):
@@ -11,8 +12,26 @@ class TodoHandler(APIView):
         todos = Todos.objects.all().order_by('-created_ts')
         for a_todo in todos:
             return_json.append({
+                'id': a_todo.id,
                 'todo': a_todo.todo,
                 'createdAt': a_todo.created_ts
             })
 
         return Response(return_json, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        todo = request.data['todo']
+        todo_obj = Todos.objects.create(
+            todo=todo
+        )
+        return Response(serializers.serialize('python', [todo_obj]), status=status.HTTP_201_CREATED)
+
+    def delete(self, request):
+        id = request.query_params['id']
+        todo_obj = Todos.objects.filter(id=id)
+
+        if todo_obj:
+            todo_obj.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
